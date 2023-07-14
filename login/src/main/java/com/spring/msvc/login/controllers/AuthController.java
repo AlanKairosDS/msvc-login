64fc781be996bbb2f7baa69dbe3b7ffd665a509f
Alan Roy Aupart Vargas <alan.aupart.vargas@kairosds.com>
2023-07-14 11:03:56 -0600

    Se modifica para que tome valores de variable de entorno

--- login/src/main/java/com/spring/msvc/login/controllers/AuthController.java
+++ login/src/main/java/com/spring/msvc/login/controllers/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:9000", maxAge = 3600, allowCredentials="true")
+@CrossOrigin(origins = "${msvclogin.app.origin}", maxAge = 3600, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
